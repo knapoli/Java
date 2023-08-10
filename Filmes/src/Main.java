a61a87e4d94951bbf4fbe3b4be8d46dde65332e1
Thiago Napoli <thiago.napoli@hotmail.com>
2023-08-10 16:33:25 -0300

    Imprimindo Informações sobre o filme

--- Filmes/src/Main.java
+++ Filmes/src/Main.java
@@ -1,17 +1,23 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Malverick");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        int anoLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoLancamento);
+        boolean incluidoNoPlano = true;
+        double notaFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                Muito bom!
+                Ano de lançamento
+                """ + anoLancamento;
+        System.out.println(sinopse);
     }
 }
