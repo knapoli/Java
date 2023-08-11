6f2c5b20cdd5fab267e21dd561e05363282c51d3
Thiago Napoli <thiago.napoli@hotmail.com>
2023-08-10 22:47:25 -0300

    Conversor de Temperatura

---
+++ Filmes/src/ConversorTemperatura.java
@@ -0,0 +1,7 @@
+public class ConversorTemperatura {
+    public static void main(String[] args) {
+        int grausCelsius = 50;
+        int grausFahrenheit = (int)(grausCelsius * 1.8) + 32;
+        System.out.println(grausCelsius + " grausCelsius " + "são " + grausFahrenheit + " grausFahrenheit");
+    }
+}
